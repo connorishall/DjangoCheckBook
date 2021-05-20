7c75b1ecd315e54a284981342c36b79f007a620b
connorishall <connorishalls@gmail.com>
2021-05-20 19:49:35 -0400

    checkbook update

--- CheckBook/Django_Checkbook/Checkbook/urls.py
+++ CheckBook/Django_Checkbook/Checkbook/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+
+
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Checkbook.urls')),
     path('', views.home, name='index'),
     path('create/', views.create_account, name='create'),
-    path('<int:pk>/balance/', views.balance, name='balance'),
+    path('/balance/', views.balance, name='balance'),
     path('transaction/', views.transaction, name='transaction')
 ]
